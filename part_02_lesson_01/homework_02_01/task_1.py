7b44978aeef3be494230a6be4574d6b8adc70df9
Andrey Samoryadov <samoryad@mail.ru>
2021-03-16 22:48:55 +0300

    part_02 homework_01 done (functions host_ping, host_range_ping, host_range_ping_tab created)

--- part_02_lesson_01/homework_02_01/task_1.py
+++ part_02_lesson_01/homework_02_01/task_1.py
@@ -8,3 +8,37 @@
 («Узел доступен», «Узел недоступен»). При этом ip-адрес
 сетевого узла должен создаваться с помощью функции ip_address().
 """
+
+from ipaddress import ip_address
+from pprint import pprint
+from subprocess import Popen, PIPE
+
+
+# функция проверки доступности сетевых узлов
+def host_ping(addr_list, timeout, requests_quantity):
+    all_addr_dict = {'Доступные узлы': '', 'Недоступные узлы': ''}
+    for address in addr_list:
+        # проверяем, это ip-адрес или доменное имя
+        try:
+            ip_addr = ip_address(address)
+        except ValueError:
+            ip_addr = address
+
+        # запускаем дочерний процесс
+        reply_from_host = Popen(f'ping {ip_addr} -w {timeout} -n {requests_quantity}', shell=False, stdout=PIPE,
+                                stderr=PIPE)
+
+        # проверяем код завершения wait, выводим результаты на экран и записываем их в словарь адресов по категориям
+        completion_code = reply_from_host.wait()
+        if completion_code == 0:
+            print(f'Узел {address} доступен')
+            all_addr_dict['Доступные узлы'] += f'{str(address)} \n'
+        else:
+            print(f'Узел {address} недоступен')
+            all_addr_dict['Недоступные узлы'] += f'{str(address)} \n'
+    return all_addr_dict
+
+
+if __name__ == '__main__':
+    addresses_list = ['192.168.1.2', '192.168.1.4', '192.168.1.5', '8.8.8.8', '127.0.0.1', 'localhost', 'www.yandex.ru']
+    pprint(host_ping(addresses_list, 500, 1))
--- part_02_lesson_01/homework_02_01/task_3.py
+++ part_02_lesson_01/homework_02_01/task_3.py
@@ -3,3 +3,15 @@
 Но в данном случае результат должен быть итоговым по всем ip-адресам, представленным в табличном формате
 (использовать модуль tabulate). Таблица должна состоять из двух колонок
 """
+from tabulate import tabulate
+from part_02_lesson_01.homework_02_01.task_2 import host_range_ping
+
+
+# функция вывода результата работы проверки доступности ip-адресов host_range_ping() в табличном виде
+def host_range_ping_tab():
+    dict_to_tab = host_range_ping()
+    print(tabulate([dict_to_tab], headers='keys', tablefmt="grid", stralign="center"))
+
+
+if __name__ == '__main__':
+    host_range_ping_tab()
