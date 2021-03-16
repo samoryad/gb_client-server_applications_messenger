63bd3202f3aaa7504a851fe07725cf94eccb2050
Andrey Samoryadov <samoryad@mail.ru>
2021-03-16 23:35:13 +0300

    part_02 homework_01 done (server realised trough class)

--- my_messenger/server.py
+++ my_messenger/server.py
@@ -60,14 +60,13 @@ def check_message_from_chat(message, CONFIGS):
         }
 
 
-# параметры командной строки скрипта server.py -p <port>, -a <addr>:
-parser = argparse.ArgumentParser(description='command line server parameters')
-parser.add_argument('-a', '--addr', type=str, default='', help='ip address')
-parser.add_argument('-p', '--port', type=int, default=CONFIGS.get('DEFAULT_PORT'), help='tcp-port')
-args = parser.parse_args()
-
-
-def main():
+# функция парсера аргументов командной строки
+def arg_parser():
+    # параметры командной строки скрипта server.py -p <port>, -a <addr>:
+    parser = argparse.ArgumentParser(description='command line server parameters')
+    parser.add_argument('-a', '--addr', type=str, default='', help='ip address')
+    parser.add_argument('-p', '--port', type=int, default=CONFIGS.get('DEFAULT_PORT'), help='tcp-port')
+    args = parser.parse_args()
     # проверка параметров вызова ip-адреса и порта из командной строки
     try:
         if '-a' or '--addr' in sys.argv:
@@ -93,87 +92,88 @@ def main():
     except ValueError:
         # print('Порт должен быть указан в пределах от 1024 до 65535')
         server_logger.critical('Порт должен быть указан в пределах от 1024 до 65535')
-        sys.exit(1)
 
-    # сервер создаёт сокет
-    sock = socket(AF_INET, SOCK_STREAM)
-    # привязывает сокет к IP-адресу и порту машины
-    sock.bind((listen_address, listen_port))
-    # готов принимать соединения
-    sock.listen(CONFIGS.get('MAX_CONNECTIONS'))
-    # Таймаут для операций с сокетом (1 секунда)
-    sock.settimeout(0.5)
+    return listen_address, listen_port
 
-    clients = []
-    messages = []
 
-    while True:
-        try:
-            # принимает запрос на установку соединения
-            client, addr = sock.accept()
-        except OSError as e:
-            pass  # timeout вышел
-        else:
-            server_logger.info(f'Установлено соединение с: {str(addr)}')
-            response = check_presence_message(get_message(client, CONFIGS), CONFIGS)
-            send_message(client, response, CONFIGS)
-            clients.append(client)
+class Server:
+    '''класс сервера'''
+    def main(self):
+        listen_address, listen_port = arg_parser()
+        # сервер создаёт сокет
+        sock = socket(AF_INET, SOCK_STREAM)
+        # привязывает сокет к IP-адресу и порту машины
+        sock.bind((listen_address, listen_port))
+        # готов принимать соединения
+        sock.listen(CONFIGS.get('MAX_CONNECTIONS'))
+        # Таймаут для операций с сокетом (1 секунда)
+        sock.settimeout(0.5)
 
-        r_list = []
-        w_list = []
-        try:
-            if clients:
-                r_list, w_list, e_list = select.select(clients, clients, [], 2)
-        except OSError:
-            # Ничего не делать, если какой-то клиент отключился
-            pass
+        clients = []
+        messages = []
 
-        # проверяем список клиентов, из которых нужно что-то прочитать
-        if r_list:
-            for client_with_message in r_list:
-                # ловим от них сообщение и проверяем его на корректность и вносим в список
-                # сообщений messages (200 или 400)
-                try:
-                    answer = check_message_from_chat(get_message(client_with_message, CONFIGS), CONFIGS)
-                    messages.append(answer)
-                    # print(messages)
-                except:
-                    server_logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера - r_list.')
-                    clients.remove(client_with_message)
+        while True:
+            try:
+                # принимает запрос на установку соединения
+                client, addr = sock.accept()
+            except OSError as e:
+                pass  # timeout вышел
+            else:
+                server_logger.info(f'Установлено соединение с: {str(addr)}')
+                response = check_presence_message(get_message(client, CONFIGS), CONFIGS)
+                send_message(client, response, CONFIGS)
+                clients.append(client)
 
-        # если есть сообщения в списке ответов после проверки (200 или 400) и есть слушающие клиенты
-        if messages and w_list:
-            # print(f' w_list --- {w_list}\n')
-            # print(f' messages --- {messages}\n')
+            r_list = []
+            w_list = []
+            try:
+                if clients:
+                    r_list, w_list, e_list = select.select(clients, clients, [], 2)
+            except OSError:
+                # Ничего не делать, если какой-то клиент отключился
+                pass
 
-            # то формируем ответное сообщение
-            message = {
-                CONFIGS['ACTION']: CONFIGS['MESSAGE'],
-                CONFIGS['TIME']: time.ctime(time.time()),
-                CONFIGS['TO_USER']: messages[0].get('to'),
-                CONFIGS['FROM_USER']: messages[0].get('from'),
-                CONFIGS['MESSAGE']: messages[0].get('message')
-            }
-            # print(message)
-            # удаляем сообщение из списка ответов после проверки
-            del messages[0]
+            # проверяем список клиентов, из которых нужно что-то прочитать
+            if r_list:
+                for client_with_message in r_list:
+                    # ловим от них сообщение и проверяем его на корректность и вносим в список
+                    # сообщений messages (200 или 400)
+                    try:
+                        answer = check_message_from_chat(get_message(client_with_message, CONFIGS), CONFIGS)
+                        messages.append(answer)
+                        # print(messages)
+                    except:
+                        server_logger.info(f'Клиент {client_with_message.getpeername()} отключился от сервера - r_list.')
+                        clients.remove(client_with_message)
 
-            # отправляем ждущим ответа клиентам сформированное сообщение
-            for waiting_client in w_list:
-                # print(f' waiting_client --- {waiting_client}\n')
-                try:
-                    send_message(waiting_client, message, CONFIGS)
-                except:
-                    server_logger.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
-                    clients.remove(waiting_client)
+            # если есть сообщения в списке ответов после проверки (200 или 400) и есть слушающие клиенты
+            if messages and w_list:
+                # print(f' w_list --- {w_list}\n')
+                # print(f' messages --- {messages}\n')
 
-        # пока оставил от урока 7
-        # requests = read_requests(r_list, clients, CONFIGS)  # Сохраним запросы клиентов
-        # if requests:
-        #     print(requests)
-        #     write_responses(requests, w_list, clients, CONFIGS)  # Выполним отправку ответов клиентам
+                # то формируем ответное сообщение
+                message = {
+                    CONFIGS['ACTION']: CONFIGS['MESSAGE'],
+                    CONFIGS['TIME']: time.ctime(time.time()),
+                    CONFIGS['TO_USER']: messages[0].get('to'),
+                    CONFIGS['FROM_USER']: messages[0].get('from'),
+                    CONFIGS['MESSAGE']: messages[0].get('message')
+                }
+                # print(message)
+                # удаляем сообщение из списка ответов после проверки
+                del messages[0]
+
+                # отправляем ждущим ответа клиентам сформированное сообщение
+                for waiting_client in w_list:
+                    # print(f' waiting_client --- {waiting_client}\n')
+                    try:
+                        send_message(waiting_client, message, CONFIGS)
+                    except:
+                        server_logger.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                        clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
     print('Стартуем сервер')
-    main()
+    server = Server()
+    server.main()
