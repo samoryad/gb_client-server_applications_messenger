900461c9fbff19c8d0d1bd6c749b84ef2564ceb0
Andrey Samoryadov <samoryad@mail.ru>
2021-04-03 22:26:21 +0300

    start_dialog.py client updated

--- my_messenger/client/start_dialog.py
+++ my_messenger/client/start_dialog.py
@@ -1,15 +1,18 @@
 from PyQt5.QtWidgets import QDialog, QPushButton, QLineEdit, QApplication, QLabel, qApp
 
 
-# Стартовый диалог с выбором имени пользователя
 class UserNameDialog(QDialog):
+    """
+    Класс реализующий стартовый диалог с запросом логина и пароля
+    пользователя.
+    """
     def __init__(self):
         super().__init__()
 
         self.ok_pressed = False
 
         self.setWindowTitle('Привет!')
-        self.setFixedSize(220, 120)
+        self.setFixedSize(218, 140)
 
         self.label = QLabel('Введите имя пользователя:', self)
         self.label.move(10, 10)
@@ -20,18 +23,27 @@ class UserNameDialog(QDialog):
         self.client_name.move(10, 30)
 
         self.btn_ok = QPushButton('Начать', self)
-        self.btn_ok.move(10, 60)
+        self.btn_ok.move(10, 105)
         self.btn_ok.clicked.connect(self.click)
 
         self.btn_cancel = QPushButton('Выход', self)
-        self.btn_cancel.move(110, 60)
+        self.btn_cancel.move(113, 105)
         self.btn_cancel.clicked.connect(qApp.exit)
+
+        self.label_passwd = QLabel('Введите пароль:', self)
+        self.label_passwd.move(10, 55)
+        self.label_passwd.setFixedSize(150, 15)
+
+        self.client_passwd = QLineEdit(self)
+        self.client_passwd.setFixedSize(200, 20)
+        self.client_passwd.move(10, 75)
+        self.client_passwd.setEchoMode(QLineEdit.Password)
 
         self.show()
 
-    # Обработчик кнопки ОК, если поле ввода не пустое, ставим флаг и завершаем приложение.
     def click(self):
-        if self.client_name.text():
+        """Метод обработчик кнопки ОК."""
+        if self.client_name.text() and self.client_passwd.text():
             self.ok_pressed = True
             qApp.exit()
 
