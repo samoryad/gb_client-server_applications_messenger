d9969b43788d50f3cede326e16975b2ea257516d
Andrey Samoryadov <samoryad@mail.ru>
2021-04-03 19:52:09 +0300

    utils.py updated

--- my_messenger/common/utils.py
+++ my_messenger/common/utils.py
@@ -4,8 +4,15 @@ import sys
 from my_messenger.common.errors import IncorrectDataReceivedError
 
 
-# функция получения и перевода сообщения из байтов с помощью json
 def get_message(opened_socket, CONFIGS):
+    """
+    Функция приёма сообщений от удалённых компьютеров.
+    Принимает сообщения JSON, декодирует полученное сообщение
+    и проверяет что получен словарь.
+    :param opened_socket: сокет для передачи данных.
+    :param CONFIGS: конфигурация.
+    :return: словарь - сообщение.
+    """
     encoded_response = opened_socket.recv(CONFIGS.get('MAX_PACKAGE_LENGTH'))
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(CONFIGS.get('ENCODING'))
@@ -16,51 +23,27 @@ def get_message(opened_socket, CONFIGS):
     raise IncorrectDataReceivedError
 
 
-# функция перевода сообщения в байты с помощью json и отправки
 def send_message(opened_socket, message, CONFIGS):
+    """
+    Функция отправки словарей через сокет.
+    Кодирует словарь в формат JSON и отправляет через сокет.
+    :param opened_socket: сокет для передачи
+    :param message: словарь для передачи
+    :param CONFIGS: конфигурация.
+    :return: ничего не возвращает
+    """
     json_message = json.dumps(message)
     encoded_message = json_message.encode(CONFIGS.get('ENCODING'))
     opened_socket.send(encoded_message)
 
 
-# функция получения словаря из json файла с настройками
 def get_configs():
+    """
+    функция получения словаря из json файла с настройками
+    """
     if not os.path.exists('common/configs.json'):
         print('Файл конфигурации не найден')
         sys.exit(1)
     with open('common/configs.json') as configs_file:
         CONFIGS = json.load(configs_file)
         return CONFIGS
-
-
-def read_requests(r_clients, all_clients, CONFIGS):
-    # Чтение запросов из списка клиентов
-    responses = {}  # Словарь ответов сервера вида {сокет: запрос}
-
-    for sock in r_clients:
-        print(sock)
-        print(r_clients)
-        try:
-            data = sock.recv(CONFIGS.get('MAX_PACKAGE_LENGTH')).decode(CONFIGS.get('ENCODING'))
-            responses[sock] = data
-        except:
-            print(f'Клиент {sock.fileno()} {sock.getpeername()} отключился')
-            all_clients.remove(sock)
-
-    return responses
-
-
-def write_responses(requests, w_clients, all_clients, CONFIGS):
-    # Эхо-ответ сервера клиентам, от которых были запросы
-
-    for sock in w_clients:
-        for _, request in requests.items():
-            try:
-                # Подготовить и отправить ответ сервера
-                resp = request.encode(CONFIGS.get('ENCODING'))
-                # Эхо-ответ сделаем чуть непохожим на оригинал
-                sock.send(resp.upper())
-            except:  # Сокет недоступен, клиент отключился
-                print(f'Клиент {sock.fileno()} {sock.getpeername()} отключился')
-                sock.close()
-                all_clients.remove(sock)
